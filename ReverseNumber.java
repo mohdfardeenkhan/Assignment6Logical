3940e571361c273783a33426315b35901b4028ac
unknown <bittufardeen@gmail.com>
2022-06-26 00:11:56 +0530

    done

---
+++ ReverseNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n,r;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter any number");
+        n=s.nextInt();
+
+        while(n>0)
+        {
+            r=n%10;
+            System.out.print(r);
+            n=n/10;
+
+        }
+    }
+
+}
